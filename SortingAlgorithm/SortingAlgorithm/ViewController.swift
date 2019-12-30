8faf38c9b5f18d40876e2aa5e695352ab4f48648
chengheming <chengheming@yingmob.com>
2019-12-30 17:44:38 +0800

    1. 冒泡排序。

--- SortingAlgorithm/SortingAlgorithm/ViewController.swift
+++ SortingAlgorithm/SortingAlgorithm/ViewController.swift
@@ -13,6 +13,15 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        var array =  [8, 1, 4, 6, 2, 3, 5, 7]
+        print("冒泡排序前: \(array)")
+        bubbling(&array)
+        let resultArr = bubbling1(array)
+        array.bubbling()
+        
+        print("冒泡排序后: \(array)")
+        print("冒泡排序后: \(resultArr)")
+        
     }
 
 
--- SortingAlgorithm/SortingAlgorithm/冒泡排序/Bubbling.swift
+++ SortingAlgorithm/SortingAlgorithm/冒泡排序/Bubbling.swift
@@ -8,3 +8,58 @@
 
 import Foundation
 
+func bubbling<T>(_ nums: inout [T]) where T: Comparable {
+    for i in 0..<(nums.count - 1) {
+        var isChange = false
+        for j in 0..<(nums.count - i - 1) {
+            if nums[j] > nums[j + 1] {
+                let temporary = nums[j]
+                nums[j] = nums[j + 1]
+                nums[j + 1] = temporary
+                
+                isChange = true
+            }
+        }
+        
+        if !isChange { break }
+    }
+}
+
+func bubbling1<T>(_ nums1: [T]) -> [T] where T: Comparable {
+    var nums = nums1
+    for i in 0..<(nums.count - 1) {
+        var isChange = false
+        for j in 0..<(nums.count - i - 1) {
+            if nums[j] > nums[j + 1] {
+                let temporary = nums[j]
+                nums[j] = nums[j + 1]
+                nums[j + 1] = temporary
+                
+                isChange = true
+            }
+        }
+        
+        if !isChange { break }
+    }
+    
+    return nums
+}
+
+public extension Array where Element: Comparable {
+     mutating func bubbling() {
+        for i in 0..<(count - 1) {
+            var isChange = false
+            for j in 0..<(count - i - 1) {
+                if self[j] > self[j + 1] {
+                    let temporary = self[j]
+                    self[j] = self[j + 1]
+                    self[j + 1] = temporary
+                    
+                    isChange = true
+                }
+            }
+            
+            if !isChange { break }
+        }
+    }
+}
