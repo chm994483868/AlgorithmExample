4aaf3d134a0ccebe03a094460818a154e98d5446
ChengHeMing <994483868@qq.com>
2020-07-20 22:54:13 +0800

    1. Review。

--- Algorithm_C/Algorithm_C/CopyComplexList/CopyComplexList.cpp
+++ Algorithm_C/Algorithm_C/CopyComplexList/CopyComplexList.cpp
@@ -269,3 +269,57 @@ void startTest_ComplexListNode_Clone() {
     Test4_ComplexListNode();
     Test5_ComplexListNode();
 }
+
+void cloneNodes_Review(ComplexListNode* pHead) {
+    ComplexListNode* pNode = pHead;
+    while (pNode != nullptr) {
+        ComplexListNode* pCloned = new ComplexListNode(pHead->m_nValue);
+        pCloned->m_nValue = pNode->m_nValue;
+        pCloned->m_pNext = pNode->m_pNext;
+        pCloned->m_pSibling = nullptr;
+        
+        pNode->m_pNext = pCloned;
+        pNode = pCloned->m_pNext;
+    }
+}
+
+void connectSiblingNodes_Review(ComplexListNode* pHead) {
+    ComplexListNode* pNode = pHead;
+    while (pNode != nullptr) {
+        ComplexListNode* pCloned = pNode->m_pNext;
+        
+        if (pNode->m_pSibling != nullptr) {
+            pCloned->m_pSibling = pNode->m_pSibling->m_pNext;
+        }
+        
+        pNode = pCloned->m_pNext;
+    }
+}
+
+ComplexListNode* reconnectNodes_Review(ComplexListNode* pHead) {
+    ComplexListNode* pNode = pHead;
+    ComplexListNode* pClonedHead = nullptr;
+    ComplexListNode* pClonedNode = nullptr;
+    
+    if (pNode != nullptr) {
+        pClonedHead = pClonedNode = pNode->m_pNext;
+        pNode->m_pNext = pClonedNode->m_pNext;
+        pNode = pNode->m_pNext;
+    }
+    
+    while (pNode != nullptr) {
+        pClonedNode->m_pNext = pNode->m_pNext;
+        pClonedNode = pClonedNode->m_pNext;
+        
+        pNode->m_pNext = pClonedNode->m_pNext;
+        pNode = pNode->m_pNext;
+    }
+    
+    return pClonedHead;
+}
+
+ComplexListNode* clone_Review(ComplexListNode* pHead) {
+    cloneNodes_Review(pHead);
+    connectSiblingNodes_Review(pHead);
+    return reconnectNodes_Review(pHead);
+}
