a67454862b67c1069f8029650e43ba2fc26bbf5a
ChengHeMing <994483868@qq.com>
2020-07-21 22:35:44 +0800

    1. 递归。

--- Algorithm_C/Algorithm_C/Review/RecursiveSummary/RecursiveSummary.cpp
+++ Algorithm_C/Algorithm_C/Review/RecursiveSummary/RecursiveSummary.cpp
@@ -8,24 +8,40 @@
 
 #include "RecursiveSummary.hpp"
 
+// 递归相关题目总结
+
 // 06. 快速排序 O(n*logn)
-void quickSort(int nums[], int l, int r) {
-    if (l >= r) {
+void quickSort(int nums[], int left, int right) {
+    if (nums == nullptr) {
         return;
     }
     
-    int i = l, j = r, x = nums[l];
+    if (left >= right) {
+        return;
+    }
+    
+    int i = left, j = right, x = nums[left];
+    
     while (i < j) {
-        while (i < j && nums[j] >= x) j--;
-        if (i < j) nums[i++] = nums[j];
+        while (i < j && nums[j] >= x) {
+            --j;
+        }
         
-        while (i < j && nums[i] < x) i++;
-        if (i < j) nums[j--] = nums[i];
+        if (i < j) {
+            nums[i++] = nums[j];
+        }
+        
+        while (i < j && nums[i] < x) {
+            ++i;
+        }
+        
+        if (i < j) {
+            nums[j--] = nums[i];
+        }
     }
     
     nums[i] = x;
     
-    // 这里的连续递归，是上下参数的值完全无相互影响的递归
-    quickSort(nums, l, i - 1);
-    quickSort(nums, i + 1, r);
+    quickSort(nums, left, i - 1);
+    quickSort(nums, i + 1, right);
 }
