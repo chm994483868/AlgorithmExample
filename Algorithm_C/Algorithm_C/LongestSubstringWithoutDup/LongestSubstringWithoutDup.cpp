39ae8bf9ebe7718e6c261bd0923253e29d474e1e
chm994483868 <994483868@qq.com>
2020-07-23 19:01:00 +0800

    1. 最长不含重复字符的子字符串。

--- Algorithm_C/Algorithm_C/LongestSubstringWithoutDup/LongestSubstringWithoutDup.cpp
+++ Algorithm_C/Algorithm_C/LongestSubstringWithoutDup/LongestSubstringWithoutDup.cpp
@@ -8,32 +8,32 @@
 
 #include "LongestSubstringWithoutDup.hpp"
 
-bool hasDuplication(const std::string& str, int position[]);
-
-int longestSubstringWithoutDuplication_1(const std::string& str) {
-    int longest = 0;
-    int* position = new int[26];
-    for (int start = 0; start < str.length(); ++start) {
-        for (int end = start; end < str.length(); ++end) {
-            int count = end - start + 1;
-            const 
-        }
-    }
-}
-
-bool hasDuplication(const std::string& str, int position[]) {
-    for (int i = 0; i < 26; ++i) {
-        position[i] = -1;
-    }
-    
-    for (int i = 0; i < str.length(); ++i) {
-        int indexInPosition = str[i] - 'a';
-        if (position[indexInPosition] >= 0) {
-            return true;
-        }
-        
-        position[indexInPosition] = indexInPosition;
-    }
-    
-    return false;
-}
+//bool hasDuplication(const std::string& str, int position[]);
+//
+//int longestSubstringWithoutDuplication_1(const std::string& str) {
+//    int longest = 0;
+//    int* position = new int[26];
+//    for (int start = 0; start < str.length(); ++start) {
+//        for (int end = start; end < str.length(); ++end) {
+//            int count = end - start + 1;
+//            const
+//        }
+//    }
+//}
+//
+//bool hasDuplication(const std::string& str, int position[]) {
+//    for (int i = 0; i < 26; ++i) {
+//        position[i] = -1;
+//    }
+//
+//    for (int i = 0; i < str.length(); ++i) {
+//        int indexInPosition = str[i] - 'a';
+//        if (position[indexInPosition] >= 0) {
+//            return true;
+//        }
+//
+//        position[indexInPosition] = indexInPosition;
+//    }
+//
+//    return false;
+//}
