0d958b1f4747c22eadfef4eabe5d94f6af7c631c
chm994483868 <994483868@qq.com>
2020-08-06 08:10:25 +0800

    1. 修改.

---
+++ Algorithm_C/Block/main.m
@@ -0,0 +1,20 @@
+//
+//  main.m
+//  Block
+//
+//  Created by CHM on 2020/8/6.
+//  Copyright © 2020 CHM. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"🎉🎉🎉 Hello, World!");
+        
+        
+        
+    }
+    return 0;
+}
--- Algorithm_C/OfferReview/01_38/19_RegularExpressionsMatching/RegularExpressions.cpp
+++ Algorithm_C/OfferReview/01_38/19_RegularExpressionsMatching/RegularExpressions.cpp
@@ -110,3 +110,16 @@ public:
         return false;
     }
 };
+
+//class Solution {
+//public:
+//    bool isMatch(string s, string p) {
+//        if(p.empty()) return s.empty();
+//        if(p[1] == '*'){
+//            return isMatch(s, p.substr(2)) || (!s.empty() && (s[0] == p[0] || p[0] == '.')) && isMatch(s.substr(1), p);
+//        }
+//        else{
+//            return !s.empty() && (s[0] == p[0] || p[0] == '.') && (isMatch(s.substr(1), p.substr(1)));
+//        }
+//    }
+//};
