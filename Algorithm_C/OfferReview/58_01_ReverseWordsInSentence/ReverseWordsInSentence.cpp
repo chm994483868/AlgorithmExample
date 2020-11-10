cb2be486c8a70067c48ee4ee68c377cf4a030d8e
chm994483868 <994483868@qq.com>
2020-11-10 18:47:54 +0800

    1. reverse.

--- Algorithm_C/OfferReview/58_01_ReverseWordsInSentence/ReverseWordsInSentence.cpp
+++ Algorithm_C/OfferReview/58_01_ReverseWordsInSentence/ReverseWordsInSentence.cpp
@@ -7,3 +7,43 @@
 //
 
 #include "ReverseWordsInSentence.hpp"
+
+// 翻转字符串，准备两个指针，一个从字符串头部开始，一个从尾部开始，
+// 左边指针递增，右边指针递减，交换两个指针指向的字符串
+void ReverseWordsInSentence::reverse(char* pBegin, char* pEnd) {
+    if (pBegin == nullptr || pEnd == nullptr) {
+        return;
+    }
+    
+    // 交换字符
+    while (pBegin < pEnd) {
+        char temp = *pBegin;
+        *pBegin = *pEnd;
+        *pEnd = temp;
+        
+        ++pBegin;
+        --pEnd;
+    }
+}
+
+char* ReverseWordsInSentence::reverseSentence(char* pData) {
+    if (pData == nullptr) {
+        return nullptr;
+    }
+    
+    char* pBegin = pData;
+    char* pEnd = pData;
+    while (*pEnd != '\0') {
+        ++pEnd;
+    }
+    --pEnd;
+    
+    // 翻转整个句子
+    reverse(pBegin, pEnd);
+    
+    // 翻转句子中的每个单词
+    pBegin = pEnd = pData;
+    while (<#condition#>) {
+        <#statements#>
+    }
+}
