e7044bf8d1cbe0c876f904c0d1c4e70e1c3e5484
chengheming <994483868@qq.com>
2021-05-08 19:10:57 +0800

    1. MergeSort.

--- Algorithm_C/OfferReview/Sort/04_SelectSort/SelectSort.cpp
+++ Algorithm_C/OfferReview/Sort/04_SelectSort/SelectSort.cpp
@@ -9,7 +9,35 @@
 #include "SelectSort.hpp"
 
 void SelectSort::selectSort(int nums[], int count) {
+    if (nums == nullptr || count <= 0) {
+        return;
+    }
+    
+    for (int i = 0; i < count; ++i) {
+        int minIndex = i;
+        for (int j = i + 1; j < count; ++j) {
+            if (nums[j] < nums[minIndex]) {
+                minIndex = j;
+            }
+        }
+        
+        swap(&nums[i], &nums[minIndex]);
+    }
+    
+    //if (nums == nullptr || count <= 0) {
+    //    return;
+    //}
     //
+    //for (int i = 0; i < count; ++i) {
+    //    int minIndex = i; // 用于记录一趟下来找到的最小元素的下标，默认 i 位置是最小元素
+    //    for (int j = i + 1; j < count; ++j) {
+    //        if (nums[j] < nums[minIndex]) {
+    //            minIndex = j; // 发现更小的元素，更新 minIndex
+    //        }
+    //    }
+    //
+    //    swap(&nums[i], &nums[minIndex]); // 一趟循环下来把最小的元素放在数列的头部
+    //}
 }
 
 // 测试代码
